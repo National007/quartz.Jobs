a18ce3bf0c2bf1a402985726710ea5ce2786e401
zhanbaohua <1591025134@qq.com>
2018-03-23 19:18:35 +0800

    第二次提交

---
+++ quartz.Jobs.Web/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using quartz.Jobs.Core.Business.Info;
+using quartz.Jobs.Core.Common;
+using System;
+using System.Collections.Concurrent;
+using System.Web.Mvc;
+
+namespace quartz.Jobs.Web.Controllers
+{
+    public class TestController : BaseController
+    {
+        
+    }
+}
